440ef3b7e67283cbc6f77cc17c8b0a4b95b1d01b
Christian <christian@Christians-MBP.fritz.box>
2026-03-17 19:29:19 +0100

    Refactor: Rename all instances of PhotoVault to PhotoVerify across the ecosystem (Android, iOS, Web, Meta)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'nl.fotolerant.photovault.ios',
+  appId: 'nl.fotolerant.photoverify.ios',
   appName: 'PhotoVerify iOS',
   webDir: 'dist',
   server: {
